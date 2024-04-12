5ec22ce2df73615bed5d73d17285f1897397ecc4
Aryan <aj619119@gmail.com>
2024-04-12 22:11:33 +0530

    make amends

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,7 @@ const axios = require("axios");
 /**
  * @type {Cypress.PluginConfig}
  */
-const baseUrl = "http://ec2-18-61-78-186.ap-south-2.compute.amazonaws.com:5000";
+const baseUrl = "http://18.61.78.186:5000";
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
